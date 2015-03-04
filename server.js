350aafd8560ef822a2467cd6985d9f3218e94ac3
Patryk Ptasiński <patryk.ptasinski@ipepe.pl>
2015-03-04 03:59:52 +0100

    server.js conficted

--- server.js
+++
@@ -1,4 +0,0 @@
-//Author: Patryk "ipepe" Ptasiński npmjs@ipepe.pl, credit to: schettino72
-// ==================== Load/start dependencies
-//this command will start server and return object with all variables that were involved in starting
-var deployd_instance = require('dpd-openshift-start-script');
